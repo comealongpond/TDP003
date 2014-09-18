7b6a774aff24625a62fecfe94003d45b2465ddc0
eried975@UND.IDA.LIU.SE <eried975@UND.IDA.LIU.SE@a58b8f34-1a94-6602-c872-f51f59ddfd82>
2014-09-18 13:32:39 +0000

    testade ta bort

--- ip1-2-4/flaskr/uppgift1.py
+++
@@ -1,7 +0,0 @@
-personlist = [{'name' :'Edling Erik','address':'Huggarvägen 45','birthdate':'19940428'},{'name' :'Carlsson Anton','address':'Herrjungavägen 2','birthdate':'19920828'},{'name' :'Adling Frank','address':'bjursätragatan 37','birthdate':'19600722'}]
-def format_by(keyn, personlist):
-    personlist = sorted(personlist, key=lambda k: k[keyn])
-    for each in personlist:
-        print(each[keyn])
-    return personlist
-
