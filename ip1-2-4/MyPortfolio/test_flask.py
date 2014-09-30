862b7923f49256bcf17f0f8bf836aacfb195af0c
eried975@UND.IDA.LIU.SE <eried975@UND.IDA.LIU.SE@a58b8f34-1a94-6602-c872-f51f59ddfd82>
2014-09-30 14:50:48 +0000

    Vi har fått sökningsfunktionen klar, filter etc ska fortfarande läggas till

--- ip1-2-4/MyPortfolio/test_flask.py
+++ ip1-2-4/MyPortfolio/test_flask.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
 import data
 
 app = Flask(__name__)
@@ -12,9 +15,29 @@ def index():
 @app.route('/techniques')
 def render_techniques():
     technique_dict = data.get_technique_stats(db)
-    project_list = data.search(db)
-    return render_template("techniques.html", title = "Tekniker", techniques = technique_dict, project = project_list)
+    return render_template("techniques.html", title = "Tekniker", techniques = technique_dict, projects = data.search(db))
     
+def search(techniqq):
+    return data.search(db, techniques=techniqq)
+
+def search_all(sort_by='start_date', sort_order='desc', techniques=None, search=None, search_fields=None):
+    return data.search(db, sort_by, sort_order, techniques, search, search_fields)
+
+@app.route('/<pid>')
+def show_project(pid):
+    project_var = data.search(db, search=str(pid), search_fields=['project_no'])
+    return render_template("project.html", title="project <pid>", projectid = pid, project = project_var)
+
+@app.route('/search', methods = ['GET', 'POST'])
+def search_page():
+    return render_template("search.html", title = "search")
+
+@app.route('/search_results', methods = ['POST'])
+def show_results():
+    return render_template("search_results.html", title = "search results", sort = request.form['sort_by'], search_text = request.form['search-text'])
+
+app.jinja_env.globals.update(search=search)
+app.jinja_env.globals.update(search_all=search_all)
 
 if __name__ == "__main__":
     app.run(debug = True)
